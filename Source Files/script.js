29238b693c8981370edccbc1d4180caccf4ebc02
Suraj Mane <68557968+mane-suraj@users.noreply.github.com>
2025-03-24 14:54:11 +0530

    Update script.js

--- Source Files/script.js
+++ Source Files/script.js
@@ -233,7 +233,8 @@ docRefData.forEach((item, index) =>{
     <td><a href="http://docinfogroupe.inetpsa.com/ead/doc/ref.${item.qiaRef}/v.vc/fiche">${item.qiaRef}</a></td>
     `;
 
-    row.style.fontSize = 2vw ? docType == "SSD";
+    if(item.docType === "SSD"){
+        $(row).css("fontSize", "0.6vw");
 
     tabBody2.append(row);
 
